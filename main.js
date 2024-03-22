536c0f1c676f8b94adf86c6c4edc2f726c3eefda
Raguf <97394996+Raguf@users.noreply.github.com>
2024-03-22 20:45:29 +0400

    Handling Click Events

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = '🎉 Correct Number';
+// document.querySelector('.message').textContent = '🎉 Correct Number';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
 
-document.querySelector('.guess').value = 23;
+// document.querySelector('.guess').value = 23;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '❌ No Number!';
+  }
+});
