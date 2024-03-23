32f87c7212eaf03ed0a04e9373f0d312f50cf59a
Raguf <97394996+Raguf@users.noreply.github.com>
2024-03-23 16:15:38 +0400

    Manipulating CSS Styles

--- main.js
+++ main.js
@@ -18,28 +18,45 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
+  // When there is not number value
   if (!guess) {
     document.querySelector('.message').textContent = '❌ No Number!';
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number';
     document.querySelector('.highscore').textContent = highScore;
+
+    // When player wins and background & color number to change color
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.color = '#60b347';
+
+    // When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '☝️ Too high';
       score--;
       document.querySelector('.score').textContent = score;
+
+      // When player lost
     } else {
       document.querySelector('.message').textContent = '😒 You lost the game!';
       document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#ff6347';
     }
+
+    // When guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '👇 Too low';
       score--;
       document.querySelector('.score').textContent = score;
+
+      // When player lost
     } else {
       document.querySelector('.message').textContent = '😒 You lost the game!';
       document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#ff6347';
     }
   }
 });
